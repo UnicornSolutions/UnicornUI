878f7a4dd018a90093e8e47bbba5c42650c9a28c
Daniel Vincent <dvincent25@gmail.com>
2024-11-08 14:28:47 -0600

    Update colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,31 @@
 import logoWhite from '../assets/Unicorn_Solutions_white.svg';
 import logoBlack from '../assets/Unicorn_Solutions_black.svg';
+import logo from '../assets/Unicorn_Solutions.svg';
 import '../App.css';
 
 const Navbar = () => (
   <>
-    <div className="navbar">
+    <div className="banner-light">
+        <img src={logo} style={{ height: 100, width: 100, verticalAlign: 'middle' }} alt="logo" />
+        <span style={{ paddingLeft: 10, color: 'white', verticalAlign: 'middle', fontSize: 50}}>Unicorn Solutions</span>
+    </div>
+    <div className="banner-light">
         <img src={logoBlack} style={{ height: 100, width: 100, verticalAlign: 'middle' }} alt="logo" />
         <span style={{ paddingLeft: 10, color: 'white', verticalAlign: 'middle', fontSize: 50}}>Unicorn Solutions</span>
     </div>
-    <div className="navbar">
+    <div className="banner-dark">
         <img src={logoWhite} style={{ height: 100, width: 100, verticalAlign: 'middle' }} alt="logo" />
         <span style={{ paddingLeft: 10, color: 'white', verticalAlign: 'middle', fontSize: 50}}>Unicorn Solutions</span>
     </div>
-    <div className="navbar2">
+    <div className="banner2-light">
+        <img src={logo} style={{ height: 100, width: 100, verticalAlign: 'middle' }} alt="logo" />
+        <span style={{ paddingLeft: 10, color: 'white', verticalAlign: 'middle', fontSize: 50}}>Unicorn Solutions</span>
+    </div>
+    <div className="banner2-light">
         <img src={logoBlack} style={{ height: 100, width: 100, verticalAlign: 'middle' }} alt="logo" />
         <span style={{ paddingLeft: 10, color: 'white', verticalAlign: 'middle', fontSize: 50}}>Unicorn Solutions</span>
     </div>
-    <div className="navbar2">
+    <div className="banner2-dark">
         <img src={logoWhite} style={{ height: 100, width: 100, verticalAlign: 'middle' }} alt="logo" />
         <span style={{ paddingLeft: 10, color: 'white', verticalAlign: 'middle', fontSize: 50}}>Unicorn Solutions</span>
     </div>
